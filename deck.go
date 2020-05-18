783cc16ae83fcb307ebb79d5ae69b2dcf7ec45bc
Guilherme Bordallo de Almeida <guilherme.almeida@gympass.com>
2020-05-18 06:56:29 -0300

    adds deal func to deck type

--- deck.go
+++ deck.go
@@ -4,8 +4,27 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{"Ace of Spades"}
+
+	cardsSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardsValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardsSuits {
+		for _, value := range cardsValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := deck{"Ace of Spades"}
-	cards.print()
+	cards := newDeck()
+	hand, cards2 := cards.deal(3)
+	fmt.Println("Hand:")
+	hand.print()
+	fmt.Println("Remaining deck:")
+	cards2.print()
 }
