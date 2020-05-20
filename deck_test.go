787007b66a509e9d5300cb2bf8bf3cf9d57b25e6
bordalloG <bordallo.guilherme@hotmail.com>
2020-05-20 06:49:16 -0300

    adds test from save to file and load from file

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -23,4 +26,18 @@ func TestNewDeck(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52 , but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
